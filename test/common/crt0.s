11d85e0bb711fc7013f9cad23c4fbb60882884b2
OmarElfouly <omarelfouly29@gmail.com>
2026-03-02 11:13:57 +0200

    Better booting

--- test/common/crt0.s
+++ test/common/crt0.s
@@ -6,38 +6,55 @@ _reset_vector:
 .section .text.init, "ax"
 .global _start
 _start:
-    /* 1. Setup the Stack Pointer */
-    la sp, _stack_top
+    /* 1. Setup the trap vector to catch exceptions/GDB breakpoints */
+    la t0, trap_handler
+    csrw mtvec, t0
 
-    /* 2. Copy the .data section from Flash (LMA) to SRAM (VMA) */
-    la a0, _sidata  /* Source: End of .text in Flash */
-    la a1, _sdata   /* Destination: Start of .data in SRAM */
-    la a2, _edata   /* End limit for SRAM .data */
-    bge a1, a2, init_bss_prep /* If size is 0, skip copy */
+    /* 2. Setup the Global Pointer (gp) with linker relaxations disabled */
+.option push
+.option norelax
+    la gp, __global_pointer$
+.option pop
+
+    /* 3. Setup the Stack Pointer (sp) and enforce 16-byte alignment */
+    la sp, _stack_top
+    andi sp, sp, -16
+
+    /* 4. Copy the .data section from Flash to SRAM */
+    la a0, _sidata
+    la a1, _sdata
+    la a2, _edata
+    bge a1, a2, init_bss_prep
 
 copy_data_loop:
-    lw t0, 0(a0)    /* Load word from Flash */
-    sw t0, 0(a1)    /* Store word to SRAM */
-    addi a0, a0, 4  /* Advance source pointer */
-    addi a1, a1, 4  /* Advance destination pointer */
+    lw t0, 0(a0)
+    sw t0, 0(a1)
+    addi a0, a0, 4
+    addi a1, a1, 4
     blt a1, a2, copy_data_loop
 
 init_bss_prep:
-    /* 3. Zero out the .bss section in SRAM */
-    la a0, _sbss    /* Start of .bss in SRAM */
-    la a1, _ebss    /* End of .bss in SRAM */
-    bge a0, a1, call_main /* If size is 0, skip zeroing */
-    li t0, 0        /* Load 0 once */
+    /* 5. Zero out the .bss section in SRAM */
+    la a0, _sbss
+    la a1, _ebss
+    bge a0, a1, call_main
+    li t0, 0
 
 zero_bss_loop:
-    sw t0, 0(a0)    /* Write 0 to SRAM */
-    addi a0, a0, 4  /* Advance pointer */
+    sw t0, 0(a0)
+    addi a0, a0, 4
     blt a0, a1, zero_bss_loop
 
 call_main:
-    /* 4. Call the C main() function */
+    /* 6. Call the C main() function */
     call main
 
-    /* 5. Trap if main ever returns */
+    /* 7. Trap if main ever returns */
 inf_loop:
     j inf_loop
+
+/* Safe landing zone for exceptions and GDB breakpoints */
+.global trap_handler
+.align 4
+trap_handler:
+    j trap_handler
